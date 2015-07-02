09a53846835840eb0f659b65c3bc0483af2e49ce
Arumugham Sankaran <arumugham@localhost.localdomain>
2015-07-02 18:03:40 +0530

    abhi design

---
+++ src/app/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('mangoLearning')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
---
+++ src/app/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('mangoLearning', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
+
+
